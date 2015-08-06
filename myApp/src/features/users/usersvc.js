714851b90f814ae408dde4b0148454405f11bb70
usagene <usagene@gmail.com>
2015-08-06 17:33:26 -0400

    Initial commit

---
+++ myApp/src/features/users/usersvc.js
@@ -0,0 +1,9 @@
+/**
+ * Created by gdai on 8/6/2015.
+ */
+export default class{
+    constructor($http){
+        "use strict";
+        this.$http = $http;
+    }
+}
---
+++ myApp/src/myApp.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gdai on 8/6/2015.
+ */
+import angular from 'angular';
+console.log(angular.version);
+angular.module('myApp, []');
